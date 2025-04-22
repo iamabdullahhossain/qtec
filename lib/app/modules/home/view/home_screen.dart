c37c4bc05802fadde5935b8222941b756d9e316f
iamabdullahhossain <abdullahhossaincse18@gmail.com>
2025-04-22 12:42:46 +0600

    Home Screen UI

--- lib/app/modules/home/view/home_screen.dart
+++ lib/app/modules/home/view/home_screen.dart
@@ -13,6 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
--- lib/app/modules/home/view/section/items_view/items_view.dart
+++ lib/app/modules/home/view/section/items_view/items_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:qtec/app/shared/controller/screen_controller/screen_controller.dart';
 
 class ItemsView extends StatefulWidget {
   const ItemsView({super.key});
@@ -10,6 +12,115 @@ class ItemsView extends StatefulWidget {
 class _ItemsViewState extends State<ItemsView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(10),
+        height: ScreenController.screenHeight,
+        width: ScreenController.screenWidth,
+        child: GridView.builder(
+          itemCount: 10,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 1.8 / 3,
+          ),
+          itemBuilder: (context, index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset("assets/image/product.png", height: 164),
+                Gap(10),
+                Text(
+                  "Allen Solly Regular fit cotton shirt",
+                  style: TextStyle(
+                    color: Color(0xFF1F2937),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Gap(8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 8,
+                  children: [
+                    Text(
+                      '\$35',
+                      style: TextStyle(
+                        color: const Color(0xFF1F2937),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        height: 1.50,
+                        letterSpacing: 0.07,
+                      ),
+                    ),
+
+                    Text(
+                      '\$40.25',
+                      style: TextStyle(
+                        color: const Color(0xFF9CA3AF),
+                        fontSize: 10,
+                        fontWeight: FontWeight.w500,
+                        decoration: TextDecoration.lineThrough,
+                        height: 2.3,
+                      ),
+                    ),
+                    Text(
+                      '15% OFF',
+                      style: TextStyle(
+                        color: const Color(0xFFEA580C),
+                        fontSize: 10,
+                        fontWeight: FontWeight.w500,
+                        height: 2.3,
+                      ),
+                    ),
+                  ],
+                ),
+                Gap(8),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 4,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 3),
+                      child: Image.asset(
+                        "assets/icon/rating.png",
+                        height: 12,
+                        width: 12,
+                      ),
+                    ),
+                    Text(
+                      '4.3',
+                      style: TextStyle(
+                        color: const Color(0xFF1F2937),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                        height: 1.50,
+                      ),
+                    ),
+                    Text(
+                      '(41)',
+                      style: TextStyle(
+                        color: const Color(0xFF6B7280),
+                        fontSize: 12,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        height: 1.50,
+                      ),
+                    ),
+
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
