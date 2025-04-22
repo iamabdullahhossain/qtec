1f3d1a3a1a0502a8eaa38bf38a6b4d7faaa4608a
iamabdullahhossain <abdullahhossaincse18@gmail.com>
2025-04-22 11:41:21 +0600

    Structure Setup & Screen Size Initialization

---
+++ lib/app/modules/home/view/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:qtec/app/modules/home/view/section/items_view/items_view.dart';
+import 'package:qtec/app/modules/home/view/section/search_view/search_view.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          spacing: 10,
+          children: [
+            SearchView(),
+            ItemsView()
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/shared/controller/screen_controller/screen_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ScreenController{
+  static late double screenHeight;
+  static late double screenWidth;
+
+  static void sizeInitialization(BuildContext context){
+    screenHeight = MediaQuery.sizeOf(context).height;
+    screenWidth = MediaQuery.sizeOf(context).width;
+  }
+
+
+}
